b2b4a48d39916d49aa5e75e92ef875b914a7ac03
jrrodas <61074904+jrrodas@users.noreply.github.com>
2021-10-13 14:37:53 -0600

    6
    6

---
+++ piedras_grandes/establecimientos/delete_est.php
@@ -0,0 +1,14 @@
+<?php 
+
+include_once '../autoridades/conexion.php';
+	if(isset($_GET['id'])){
+		$id=(int) $_GET['id'];
+		$delete=$con->prepare('DELETE FROM establecimientos_piedras_grandes WHERE id=:id');
+		$delete->execute(array(
+			':id'=>$id
+		));
+		header('Location: establecimientos.php');
+	}else{
+		header('Location: establecimientos.php');
+	}
+ ?>
---
+++ platanitos/poblacion/delete_pob.php
@@ -0,0 +1,14 @@
+<?php 
+
+include_once '../autoridades/conexion.php';
+	if(isset($_GET['id'])){
+		$id=(int) $_GET['id'];
+		$delete=$con->prepare('DELETE FROM poblacion_platanitos WHERE id=:id');
+		$delete->execute(array(
+			':id'=>$id
+		));
+		header('Location: poblacion.php');
+	}else{
+		header('Location: poblacion.php');
+	}
+ ?>
